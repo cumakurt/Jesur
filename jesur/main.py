39367c3796ec5f877f0616c60662bb741b5391a9
Cuma KURT <91676004+cumakurt@users.noreply.github.com>
2025-12-10 20:45:57 +0300

    Update main.py

--- jesur/main.py
+++ jesur/main.py
@@ -2,7 +2,7 @@
 JESUR - Enhanced SMB Share Scanner
 Main application module
 
-Developer: cumakurt
+Developer: Cuma Kurt
 GitHub: https://github.com/cumakurt/Jesur
 LinkedIn: https://www.linkedin.com/in/cuma-kurt-34414917/
 Version: 2.0.0
