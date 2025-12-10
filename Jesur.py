e3741ebb13cf4fbba31c4505d5e45195dc06750c
Cuma KURT <91676004+cumakurt@users.noreply.github.com>
2025-12-10 20:45:01 +0300

    Update Jesur.py

--- Jesur.py
+++ Jesur.py
@@ -3,7 +3,7 @@
 JESUR - Enhanced SMB Share Scanner
 Main entry point
 
-Developer: cumakurt
+Developer: Cuma KURT
 GitHub: https://github.com/cumakurt/Jesur
 LinkedIn: https://www.linkedin.com/in/cuma-kurt-34414917/
 Version: 2.0.0
